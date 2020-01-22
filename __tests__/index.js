d982f8b368d61cc1d72c635ec182fea2728cf4aa
Paulius Varna <paul.varna@gmail.com>
2020-01-22 16:50:04 +0200

    secrets

--- __tests__/index.js
+++ __tests__/index.js
@@ -3,16 +3,11 @@ const {
   languages,
   projectToFiles
 } = require('../dist/fetch-poeditor.cjs.js')
-console.error(process.env.POEDITOR)
-const api_token = process.env.POEDITOR
-const id = '108369'
 
 test('singleJSON', () => {
   expect.assertions(1)
 
   return singleJSON({
-    api_token,
-    id,
     language: 'lt',
     // type: 'key_value_json',
     // filters: ['translated', 'proofread'],
@@ -26,10 +21,8 @@ test('singleJSON', () => {
 
 test('languages', () => {
   expect.assertions(1)
-
+  
   return languages({
-    api_token,
-    id
   })
     .then(data => {
       expect(data)
@@ -41,8 +34,6 @@ test('projectToFiles', () => {
   expect.assertions(1)
 
   return projectToFiles({
-    api_token,
-    id,
     path: './ddd'
   })
     .then(data => {
